cf3dc8f3904947b77663f7c972f80235288fc17f
Carson Graham <carson42g@gmail.com>
2021-11-21 03:35:09 -0500

    started working on c interop

--- code/asm.s
+++ code/asm.s
@@ -1,9 +1,43 @@
 
 .global _start
 
+
+.thumb
 .section .text
 
 _start:
+
+
+// r15 = PC program counter
+// r14 = LR link register 
+// r13 = SP stack pointer
+// r12 = IP intra-procedure-call scratch register
+// r11 = FP frame pointer
+
+// r0-r3   argument registers
+// r4-r8   callee-saved
+// r9      is cringe
+// r10-r11 callee-saved
+// r12-15  special
+
+// working memory starts at 0x20000000
+// and              ends at 0x20003FFF
+// so I supposed we will set the stack pointer
+// to 0x20003FFF and see how it goes. Maybe we want 0x200003FF0 to pad for qwords, or 0x200004000 cause it subrtacts then moves down
+ldr r0, =0x20003FF
+mov sp, r0
+
+mov r0, #0x77
+mov r1, #0x77
+mov r2, #0x77
+
+push {r0}
+push {r1}
+push {r2}
+
+mov r0, r0
+mov r0, r0
+blinkLight:
 
 @ set PA27 to OUT
 ldr r0, =0x41004400 @ this is the location of the POST tables
---
+++ code/main.c
@@ -0,0 +1,11 @@
+
+// memory arangement
+
+
+int getNumber() {
+    return 0xBABEBABE;
+}
+
+int getNumber2() {
+    return getNumber() + 1;
+}
